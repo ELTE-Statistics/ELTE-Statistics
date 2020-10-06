42a7287fd337213795e24933652690a987f17e37
Su <su@sus-macbook-pro.home>
2020-10-06 17:30:33 +0200

    add StatisticsController

--- src/main/java/hu/elte/inf/statistics/ElteStatisticsApplication.java
+++ src/main/java/hu/elte/inf/statistics/ElteStatisticsApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElteStatisticsApplication {
+public class
+ElteStatisticsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElteStatisticsApplication.class, args);
---
+++ src/main/java/hu/elte/inf/statistics/controller/StatisticsController.java
@@ -0,0 +1,16 @@
+package hu.elte.inf.statistics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class StatisticsController {
+    @GetMapping("/statistics")
+    public ModelAndView render(HttpServletRequest req) {
+        ModelAndView mv = new ModelAndView("statistics");
+        return mv;
+    }
+}
