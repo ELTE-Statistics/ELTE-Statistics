8c9c566462f76ad0277fd97de34b95a548fa85b2
botko07 <botko.gio@gmail.com>
2020-11-14 23:13:11 +0400

    Setters and Getters for StaffDAO

--- src/main/java/hu/elte/inf/statistics/DAO/StaffDAO.java
+++ src/main/java/hu/elte/inf/statistics/DAO/StaffDAO.java
@@ -55,5 +55,219 @@ public class StaffDAO {
     }
 
     // TODO: add getters and setters for each staff attribute.
+    public int getCommunicationDataCount(String staffName){
+        String query = "select communication_count from staff where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setString(1,staffName);
+            res = st.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        int val = -1;
+        try {
+            if(res.next())
+                val = res.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            if(res != null)
+                res.close();
+            if(st != null)
+                st.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return val;
+    }
+    public void setCommunicationDataCount(String staffName, int communicationData){
+        String query = "update  staff set communication_count = ? where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setInt(1, communicationData);
+            st.setString(2, staffName);
+            st.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        if(st != null) {
+            try {
+                st.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+    public int getTeachingDataCount(String staffName){
+        String query = "select teaching_count from staff where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setString(1,staffName);
+            res = st.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        int val = -1;
+        try {
+            if(res.next())
+                val = res.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            if(res != null)
+                res.close();
+            if(st != null)
+                st.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return val;
+    }
+
+    public void setTeachingDataCount(String staffName, int teachingData){
+        String query = "update  staff set teaching_count = ? where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setInt(1, teachingData);
+            st.setString(2, staffName);
+            st.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        if(st != null) {
+            try {
+                st.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+    public double getAverageCommunicationSkills(String staffName){
+        String query = "select average_communication from staff where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setString(1,staffName);
+            res = st.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        double val = -1;
+        try {
+            if(res.next())
+                val = res.getDouble(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            if(res != null)
+                res.close();
+            if(st != null)
+                st.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return val;
+    }
+
+    public void setAverageCommunicationSkills(String staffName, double averageCommunicationSkills){
+        String query = "update  staff set average_communication = ? where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setDouble(1, averageCommunicationSkills);
+            st.setString(2,staffName);
+            st.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        if(st != null) {
+            try {
+                st.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+
+    public double getAverageTeachingQuality(String staffName){
+        String query = "select average_teaching from staff where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setString(1,staffName);
+            res = st.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        double val = -1;
+        try {
+            if(res.next())
+                val = res.getDouble(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            if(res != null)
+                res.close();
+            if(st != null)
+                st.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return val;
+    }
+
+    public void setAverageTeachingQuality(String staffName, double averageTeachingQuality){
+        String query = "update  staff set average_teaching = ? where full_name = ?";
+        PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+            st = conn.prepareStatement(query);
+            st.setDouble(1, averageTeachingQuality);
+            st.setString(2,staffName);
+            st.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        if(st != null) {
+            try {
+                st.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
 
 }
