5131868dec25a0491a2f00517f620121dba4c962
Shiny-Su <suxiaotian31@gmail.com>
2020-12-10 19:40:30 +0100

    Add rating professor page

---
+++ src/main/java/hu/elte/inf/statistics/Controllers/RateProfessorController.java
@@ -0,0 +1,57 @@
+package hu.elte.inf.statistics.Controllers;
+
+import hu.elte.inf.statistics.DAO.CoursesDAO;
+import hu.elte.inf.statistics.Models.CourseReport;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/** Rate Professor controller */
+@Controller
+public class RateProfessorController {
+
+    /**
+     * @param req
+     * @return modelAndView object
+     */
+    @GetMapping("/rateProfessor")
+    public ModelAndView render(HttpServletRequest req) {
+        ModelAndView mv = new ModelAndView("rateProfessor");
+        return mv;
+    }
+
+    /**
+     * updates modelAndView
+     *
+     * @param courseName
+     * @param difficulty
+     * @param usefulness
+     * @param comment
+     * @param button
+     * @param req
+     * @return modelAndView object
+     */
+    @PostMapping("/rateProfessor")
+    public ModelAndView update(
+            @RequestParam String courseName,
+            @RequestParam Integer difficulty,
+            @RequestParam Integer usefulness,
+            @RequestParam String comment,
+            @RequestParam String button,
+            HttpServletRequest req) {
+
+        if (button.equals("submit-bt")) {
+            CourseReport courseReport =
+                    new CourseReport(courseName, comment, difficulty, usefulness);
+            CoursesDAO coursesDAO = new CoursesDAO();
+            coursesDAO.addCourseReport(courseReport);
+            req.setAttribute("txt", "Review has been submitted.");
+        }
+
+        return new ModelAndView("rateProfessor");
+    }
+}
