0feb9945576aa92f4c4b363d584ee1bf0cd0387c
Szumi Xie <szumixie@gmail.com>
2020-12-10 22:26:14 +0100

    Add professor table

--- src/main/java/hu/elte/inf/statistics/Controllers/StatisticsController.java
+++ src/main/java/hu/elte/inf/statistics/Controllers/StatisticsController.java
@@ -1,6 +1,7 @@
 package hu.elte.inf.statistics.Controllers;
 
 import hu.elte.inf.statistics.DAO.CoursesDAO;
+import hu.elte.inf.statistics.DAO.StaffDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,10 @@ public class StatisticsController {
     public String getData(Model model) {
         CoursesDAO coursesDAO = new CoursesDAO();
         model.addAttribute("courses", coursesDAO.getAll());
+
+        StaffDAO staffDAO = new StaffDAO();
+        model.addAttribute("professors", staffDAO.getAll());
+
         return "statistics";
     }
 }
